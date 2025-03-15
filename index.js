580ee9ea6dc8fa670d90d4c2974bd34c575ca3f5
+ <+>
2025-03-15 12:12:07 +0300

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
-// Write your code here
+// Write your code here// Multiply num1 and num2 to get 62
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;  // Result: 62
+
+// Generate a random number greater than 0
+let random = Math.floor(Math.random() * 100) + 1;  // Random number greater than 0
+
+// Calculate the remainder of num3 divided by num4, the remainder should be 4
+let num3 = 14;
+let num4 = 10;
+let mod = num3 % num4;  // Result: 4
+
+// Find the maximum number in the set, should return 20
+let max = Math.max(10, 15, 20, 5, 3);  // Result: 20
+
+
+module.exports = {
+  multiply,
+  random,
+  mod,
+  max
+};
